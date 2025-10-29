a2ebaad699d3eba88c73529fadfbeba251bb8144
Vansh Sahu <vanshsahu6752@gmail.com>
2025-10-29 22:11:48 +0530

    Basic routing setup and testing

--- src/app.js
+++ src/app.js
@@ -5,28 +5,29 @@ import cors from "cors";
 
 const app = express();
 
+//Middlewares
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
-
 app.use(
   express.json({
     limit: limit,
   })
 );
-
 app.use(express.static("public"));
-
 app.use(
   express.urlencoded({
     extended: true,
     limit: limit,
   })
 );
-
 app.use(cookieParser());
+
+//Routes
+import userRouter from "./routes/user.routes.js"
+app.use("/users",userRouter)
 
 export { app };
